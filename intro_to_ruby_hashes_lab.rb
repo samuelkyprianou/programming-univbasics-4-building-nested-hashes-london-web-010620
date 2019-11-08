f31df1b0879b75af82a8d688ad3bc023fd42e36a
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-08 16:55:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        one: {},
+        one_piece_owned: 25,
         two: {},
         three: {},
         four: {}
